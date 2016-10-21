8d0acb4b59143b8760c46fb28ac4c8e712479d6d
Semenova <ansemen@merann.ru>
2016-10-21 14:54:40 +0300

    HomeWork5: person info in xhtml

---
+++ semenova/5/src/main/java/logic/model/User.java
@@ -0,0 +1,53 @@
+package logic.model;
+
+/**
+ * Created by ansemen on 06.10.2016.
+ */
+public class User {
+
+    Long id;
+    String firstName;
+    String secondName;
+    String password;
+    String photo;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getSecondName() {
+        return secondName;
+    }
+
+    public void setSecondName(String secondName) {
+        this.secondName = secondName;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getPhoto() {
+        return photo;
+    }
+
+    public void setPhoto(String photo) {
+        this.photo = photo;
+    }
+}
