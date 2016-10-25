6598d9bbb340534d87f318b867a7bddbc8eda60d
Sergey <sergvild@mail.ru>
2016-10-25 17:58:18 +0300

    Lab2

---
+++ vildanov/2/src/main/java/MainServlet.java
@@ -0,0 +1,25 @@
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.PrintWriter;
+
+/**
+ * Created by svildan on 05.10.2016.
+ */
+public class MainServlet extends HttpServlet {
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp)
+            throws ServletException, IOException {
+        PrintWriter out = resp.getWriter();
+      out.println(req.getAttribute("text"));
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp)
+            throws ServletException, IOException {
+
+        super.doPost(req, resp);
+    }
+}
