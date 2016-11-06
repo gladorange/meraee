db6cf16276e2d99f5fdcf22281ef4668beda15e6
ostrovsk <ostrovsk@merann.ru>
2016-11-06 22:08:10 +0300

    Homework No. 2 as a Maven project

---
+++ ostrovskiy/2/app02/src/main/java/org/arvios/myapps/Replacer.java
@@ -0,0 +1,39 @@
+package org.arvios.myapps;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.Servlet;
+import javax.servlet.ServletConfig;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Created by artem-ostrovsky on 06.11.2016.
+ */
+public class Replacer extends HttpServlet {
+    private static final long serialVersionUID = 1L;
+
+    /**
+     * @see Servlet#init(ServletConfig)
+     */
+    public void init(ServletConfig config) throws ServletException {
+        // TODO Auto-generated method stub
+    }
+
+    /**
+     * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+     */
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        // TODO Auto-generated method stub
+        request.setCharacterEncoding("UTF-8");
+        response.setCharacterEncoding("UTF-8");
+        String text = request.getParameter("text");
+        response.setContentType("text/html");
+        PrintWriter out = response.getWriter();
+        out.println(text);
+    }
+
+}
